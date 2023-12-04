274d3a0537f612d539170aed758b3ce34515f80a
unknown <riasat01@outlook.com>
2023-12-04 23:32:08 +0600

    bug fixs

--- src/models/Donator.js
+++ src/models/Donator.js
@@ -14,6 +14,10 @@ const DonatorSchema = new Schema({
         type: String,
         required: true
     },
+    "donatedAmount": {
+        type: Number,
+        required: true
+    },
     "donation": DonationSchema
 })
 
